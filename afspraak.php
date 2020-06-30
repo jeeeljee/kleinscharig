e048bb24e13b9332518a411177d1a5b2eac89fce
jeeeljee <jeeeljee@gmail.com>
2020-06-30 22:10:11 +0200

    Update afspraak.php
    Terug naar oude widget

--- afspraak.php
+++ afspraak.php
@@ -13,10 +13,40 @@
                     <div class="row">
                         <div class="col-sm-10 col-md-8">
                             <h1>Maak een afspraak</h1>
-                            <p class="lead"> Ben je nieuw bij Kleinscharig? Dan kan je terecht bij <a href="over-ons#jessica" style="line-height: 0;">Jessica</a>, <a href="over-ons#michelle" style="line-height: 0;">Michelle</a> of <a href="over-ons#natascha" style="line-height: 0;">Natascha</a>. Momenteel is er bij <a href="over-ons#barbara" style="line-height: 0;">Barbara</a> geen plek meer. Je kan hieronder gemakkelijk een afspraak maken, we sturen je een bevestiging via de e-mail. </p>
-	                        	<script src="https://static-widget.salonized.com/loader.js"></script>	
-							<div class="salonized-booking" data-company="4--0LrE" data-color="#c75146" data-language="nl" data-height="620" data-inline="true"></div>
+                            <p class="lead"> Ben je nieuw bij Kleinscharig? Dan kan je terecht bij <a href="over-ons#jessica" style="line-height: 0;">Jessica</a>, <a href="over-ons#michelle" style="line-height: 0;">Michelle</a> of <a href="over-ons#natascha" style="line-height: 0;">Natascha</a>. Momenteel is er bij <a href="over-ons#barbara" style="line-height: 0;">Barbara</a> geen plek meer. Je kan hieronder gemakkelijk een afspraak maken, we sturen je een bevestiging via de e-mail. </p>	                        
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section class="switchable switchable--switch text-justify">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-5"> 
+
+                            <script language="JavaScript">
+                            <!--
+                            
+                            function random_imglink(){
+                            var myimages=new Array()
+                            myimages[1]="img/afspraak/kleinscharig-kapsalon-amersfoort-barbara.jpg"         
+                            myimages[2]="img/afspraak/kleinscharig-kapsalon-amersfoort-barbara2.jpg"                
+                            myimages[3]="img/afspraak/kleinscharig-kapsalon-amersfoort-jessica.jpg" 
+                            myimages[4]="img/afspraak/kleinscharig-kapsalon-amersfoort-jessica2.jpg"
+                            myimages[5]="img/afspraak/kleinscharig-kapsalon-amersfoort-barbara3.jpg"
+                            myimages[6]="img/afspraak/kleinscharig-kapsalon-amersfoort-team.jpg"                                                                                                                                                                    
+                            var ry=Math.floor(Math.random()*myimages.length)
+                            if (ry==0)
+                            ry=1
+                            document.write('<img alt="afspraak kleinscharig" class="border--round box-shadow-wide" src="'+myimages[ry]+'" border=0>')
+                            }
+                            random_imglink()
+                            //-->
+                            </script>
+                        </div>  
+                        <div class="col-sm-6">
+                            <iframe src="//kleinscharig.salonized.com/bookings/new?layout=embed" style="height:620px;" class="salonized"></iframe>  
+                        </div>
+
                     </div>
                 </div>
             </section>
